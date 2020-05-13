e91189387ffb220ea75ff5c57973ee3228ff3d66
Abhishek Bindra <abhishek.bindra.ab@gmail.com>
2020-05-13 20:02:16 +0530

    Phase 1

---
+++ server.js
@@ -0,0 +1,49 @@
+const express = require('express')
+const app = express()
+const port = 8000
+var multer = require('multer')
+var cors = require('cors')
+var bodyParser = require('body-parser')
+var fs = require('fs')
+
+app.use(cors())
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 }))
+
+// parse application/json
+app.use(bodyParser.json({ limit: "50mb" }))
+
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './uploads')
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname + '-' + Date.now() + '.mp4')
+    }
+})
+
+var upload = multer({ storage: storage }).single('file')
+
+app.post('/upload', (req, res) => {
+    // upload(req, res, function (err) {
+    //     if (err instanceof multer.MulterError) {
+    //         console.log("Err1: " + err);
+    //     } else if (err) {
+    //         console.log("Err2: " + err);
+    //     }
+    //     return res.status(200).send('Success!');
+    // })
+    // console.log(req.body.data);
+    req.body.data = req.body.data.replace(/^data:(.*?);base64,/, "");
+    req.body.data = req.body.data.replace(/ /g, '+');
+
+    fs.appendFile(`./uploads/sample.mp4`, req.body.data, 'base64', function (err) {
+        if (err) console.log(err);
+    });
+    res.status(200).send('Success!');
+})
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(port, () => console.log(`Server listening at http://localhost:${port}`))
