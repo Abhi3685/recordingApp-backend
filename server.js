c60a428625a769e6744573c5ec97e62f8679ff73
Abhishek Bindra <abhishek.bindra.ab@gmail.com>
2020-05-14 12:24:49 +0530

    Player Added

--- server.js
+++ server.js
@@ -17,11 +17,42 @@ app.post('/upload', (req, res) => {
     req.body.data = req.body.data.replace(/^data:(.*?);base64,/, "");
     req.body.data = req.body.data.replace(/ /g, '+');
 
-    fs.appendFile(`./uploads/sample.mp4`, req.body.data, 'base64', function (err) {
+    fs.appendFile(`./uploads/userid-timestamp.mp4`, req.body.data, 'base64', function (err) {
         if (err) console.log(err);
     });
     res.status(200).send('Success!');
 })
+
+app.get('/video', function (req, res) {
+    const path = './uploads/userid-timestamp.mp4'
+    const stat = fs.statSync(path)
+    const fileSize = stat.size
+    const range = req.headers.range
+    if (range) {
+        const parts = range.replace(/bytes=/, "").split("-")
+        const start = parseInt(parts[0], 10)
+        const end = parts[1]
+            ? parseInt(parts[1], 10)
+            : fileSize - 1
+        const chunksize = (end - start) + 1
+        const file = fs.createReadStream(path, { start, end })
+        const head = {
+            'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+            'Accept-Ranges': 'bytes',
+            'Content-Length': chunksize,
+            'Content-Type': 'video/mp4',
+        }
+        res.writeHead(206, head);
+        file.pipe(res);
+    } else {
+        const head = {
+            'Content-Length': fileSize,
+            'Content-Type': 'video/mp4',
+        }
+        res.writeHead(200, head)
+        fs.createReadStream(path).pipe(res)
+    }
+});
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
