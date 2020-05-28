583e9209ae4a087751f9768edc2968aec539e9d5
Abhishek Bindra <abhishek.bindra.ab@gmail.com>
2020-05-28 13:03:09 +0530

    Progress

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const app = express()
 const port = 8000
 var cors = require('cors')
 var multer = require('multer')
-var sharp = require('sharp')
 var bodyParser = require('body-parser')
 var fs = require('fs')
 const execFile = require('child_process').execFile;
@@ -12,8 +11,6 @@ var storage = multer.diskStorage(
     {
         destination: './uploads/',
         filename: function (req, file, cb) {
-            //req.body is empty...
-            //How could I get the new_file_name property sent from client here?
             cb(null, file.originalname + ".png");
         }
     }
@@ -95,34 +92,6 @@ app.post('/trim', (req, res) => {
             });
         });
     }
-})
-
-app.post('/watermark', upload.single('logo'), (req, res) => {
-
-    sharp("./uploads/" + req.body.userId + ".png")
-        .resize({ width: 200 })
-        .toFile("./uploads/" + req.body.userId + "-2.png")
-        .then(() => {
-            fs.unlink("./uploads/" + req.body.userId + ".png", () => { });
-            // Add New Logo To Video
-            const str = "[1]lut=a=val*0.8[a];[0][a]overlay=main_w-overlay_w-20:main_h-overlay_h-30";
-            execFile('ffmpeg', ['-i', req.body.vid_url, '-i', "./uploads/" + req.body.userId + "-2.png", '-filter_complex', str, '-preset', "ultrafast", '-c:a', 'copy', "./uploads/" + req.body.userId + ".mp4"], (error, stdout, stderr) => {
-                if (error) return res.send(error);
-                fs.unlink("./uploads/" + req.body.userId + "-2.png", () => { });
-                // Upload new video to cloudinary
-                cloudinary.api.delete_resources([req.body.pid],
-                    { resource_type: "video" }
-                );
-                cloudinary.uploader.upload('./uploads/' + req.body.userId + '.mp4',
-                    { resource_type: "video" },
-                    function (err, video) {
-                        if (err) return res.send(err)
-                        fs.unlink('./uploads/' + req.body.userId + '.mp4', () => { });
-                        res.send({ ...video, code: 'Success' });
-                    });
-            });
-        });
-
 })
 
 app.get('/', (req, res) => {
